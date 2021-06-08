ad32b3594286872b5585b531eb3c3a4caf2a0856
isjohndepressed <85541531+isjohndepressed@users.noreply.github.com>
2021-06-08 11:32:48 -0700

    updates

---
+++ set.sh
@@ -0,0 +1,5 @@
+git reset --hard HEAD
+git pull
+cat index.html.$1 > index.html
+git commit -am "depressed 😔"
+git push
--- toggle.sh
+++
@@ -1,8 +0,0 @@
-git reset --hard HEAD
-git pull
-mv index.html index.html.off.temp
-mv index.html.off index.html
-mv index.html.off.temp index.html.off
-git commit -am "toggled"
-git push
-exit 0
